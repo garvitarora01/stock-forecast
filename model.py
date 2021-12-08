5c79160cd2a19aec21870677a4f85d43445ef2d9
Garvit Arora <59766505+garvitarora01@users.noreply.github.com>
2021-12-08 09:18:37 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 def prediction(stock, n_days):
-    import 
     import dash
     import dash_core_components as dcc
     import dash_html_components as html
@@ -12,21 +11,102 @@ def prediction(stock, n_days):
     import plotly.express as px
     # model
     from model import prediction
-    from fbprophet import Prophet
-    from fbprophet.plot import plot_plotly
-    from plotly import graph_objs as go
+    from sklearn.model_selection import train_test_split
+    from sklearn.model_selection import GridSearchCV
+    import numpy as np
+    from sklearn.svm import SVR
+    from datetime import date, timedelta
+    from sklearn.preprocessing import MinMaxScaler
     # load the data
-    
-    df = yf.download(stock,period='30d')
+
+    df = yf.download(stock, period='100d')
     df.reset_index(inplace=True)
-    df_train = df[['Date','Close']]
-    df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+    #df['Day'] = df.index
+    df.drop(['Date'],inplace=True,axis=1)
 
-    m = Prophet(daily_seasonality=True,growth='linear')
-    m.fit(df_train)
-    future = m.make_future_dataframe(periods=n_days)
-    forecast = m.predict(future)
-    fig1 = plot_plotly(m, forecast)
+    numeric = ['Open', 'High', 'Low', 'Adj Close', 'Volume']
+    scale = MinMaxScaler().fit(df[numeric])
+    df[numeric] = scale.fit_transform(df[numeric])
 
+    days = list()
+    for i in range(len(df.Day)):
+         days.append([i])
 
-    return fig1
+    # Splitting the dataset
+
+    X = df[numeric]
+    Y = df[['Close']]
+
+    x_train, x_test, y_train, y_test = train_test_split(X,
+                                                        Y,
+                                                        test_size=0.1,
+                                                        shuffle=False)
+
+    gsc = GridSearchCV(
+        estimator=SVR(kernel='rbf'),
+        param_grid={
+            'C': [0.001, 0.01, 0.1, 1, 100, 1000],
+            'epsilon': [
+                0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10,
+                50, 100, 150, 1000
+            ],
+            'gamma': [0.0001, 0.001, 0.005, 0.1, 1, 3, 5, 8, 40, 100, 1000]
+        },
+        cv=5,
+        scoring='neg_mean_absolute_error',
+        verbose=0,
+        n_jobs=-1)
+
+    y_train = y_train.values.ravel()
+    y_train
+    grid_result = gsc.fit(x_train, y_train)
+    best_params = grid_result.best_params_
+    best_svr = SVR(kernel='rbf',
+                   C=best_params["C"],
+                   epsilon=best_params["epsilon"],
+                   gamma=best_params["gamma"],
+                   max_iter=-1)
+
+    # Support Vector Regression Models
+
+    rbf_svr = best_svr
+
+    rbf_svr.fit(x_train, y_train)
+
+    output_days = list()
+    for i in range(1, n_days):
+        output_days.append([i + x_test[-1][0]])
+
+    dates = []
+    current = date.today()
+    for i in range(n_days):
+        current += timedelta(days=1)
+        dates.append(current)
+
+    # plot Results
+    # fig = go.Figure()
+    # fig.add_trace(
+    #     go.Scatter(x=np.array(x_test).flatten(),
+    #                y=y_test.values.flatten(),
+    #                mode='markers',
+    #                name='data'))
+    # fig.add_trace(
+    #     go.Scatter(x=np.array(x_test).flatten(),
+    #                y=rbf_svr.predict(x_test),
+    #                mode='lines+markers',
+    #                name='test'))
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=dates,  # np.array(ten_days).flatten(), 
+            y=rbf_svr.predict(output_days),
+            mode='lines+markers',
+            name='data'))
+    fig.update_layout(
+        title="Predicted Close Price of next " + str(n_days - 1) + " days",
+        xaxis_title="Date",
+        yaxis_title="Closed Price",
+        # legend_title="Legend Title",
+    )
+
+    return fig
